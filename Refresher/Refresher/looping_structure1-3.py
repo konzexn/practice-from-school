f98266fa3fd1cbdcd074fe69fb94788090c1ae11
konzexn <konzexn@gmail.com>
2024-05-31 17:30:11 +0800

    add

--- Refresher/Refresher/looping_structure1-3.py
+++
@@ -1,9 +0,0 @@
-while True:
-    num = input("Enter your age: ")
-    if int(num) < 18:
-        print("You are too young.")
-    elif int(num) > 18:
-        print("You are too old.")
-
-
-        
--- Refresher/Refresher/looping_structure1.py
+++
@@ -1,5 +0,0 @@
-num = float(input("enter the number: "))
-if 0:
-    print("is negative number")
-else:
-    print("is a positive number")
