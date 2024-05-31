2f4af676677b809207fa03fb92f9d9a4f7738e5d
konzexn <konzexn@gmail.com>
2024-05-31 17:32:53 +0800

    add

---
+++ python lib/looping_structure1-2.py
@@ -0,0 +1,4 @@
+num = int(input("enter the number 1 to 10: "))
+for i in range(1,11):
+    print(f"{num} x {i} = {num * i}")
+    
---
+++ python lib/looping_structure1-3.py
@@ -0,0 +1,9 @@
+while True:
+    num = input("Enter your age: ")
+    if int(num) < 18:
+        print("You are too young.")
+    elif int(num) > 18:
+        print("You are too old.")
+
+
+        
